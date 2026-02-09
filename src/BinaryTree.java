46439e0baf49bc91eeabdd0b317fc130b8e8cce0
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-02-09 17:59:35 +0530

    "levelorder"

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class BinaryTree {
     static class Node{
         int data;
@@ -52,6 +54,35 @@ public class BinaryTree {
                   inorder(root.right);
     }
 
+        public static void levelorder(Node root) {
+            if (root == null) return;
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while (!q.isEmpty()) {
+                Node currNode = q.remove();
+
+                if (currNode == null) {
+                    System.out.println();
+                    if (q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(currNode.data + " ");
+
+                    if (currNode.left != null)
+                        q.add(currNode.left);
+
+                    if (currNode.right != null)
+                        q.add(currNode.right);
+                }
+            }
+        }
+
     }
     public static void main(String[] args) {
         int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -61,6 +92,7 @@ public class BinaryTree {
         System.out.println(root.data);
       //  tree.preorder(root);
 //        tree.postorder(root);
-      tree.inorder(root);
+     // tree.inorder(root);
+        tree.levelorder(root);
     }
 }
