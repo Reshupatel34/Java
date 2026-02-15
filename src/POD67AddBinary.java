f6f6b563fa35032c52fc7878661ed1a0fbed2e60
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-02-16 03:05:49 +0530

    "add binary"

---
+++ src/POD67AddBinary.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class POD67AddBinary {
+    public static void main(String[] args) {
+        String a="11";
+        String b="1";
+        StringBuilder sb = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            int sum = carry;
+
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+
+
+        System.out.println(sb.reverse().toString());
+
+    }
+}
