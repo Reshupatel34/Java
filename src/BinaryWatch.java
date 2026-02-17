beffd13e5fdd73b9a1a7b8099a70f6a87d83aa9e
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-02-18 02:04:59 +0530

    "binary watch leetcode pod"

--- src/BinaryWatch.java
+++ src/BinaryWatch.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryWatch {
     public static void main(String[] args) {
 
+        int turnedOn=2;
+        //the hours are from 0 to 11
+        // and the minutes are from 0 to 59
+        // so the time complexity would be O(12+60)
+        List<String> time=new ArrayList<>();
+        for(int hours=0;hours<=11;hours++){
+            // check every possible combination for the hours and minutes
+            for(int minutes=0;minutes<=59;minutes++){
+                // the total number of set bits of hours and minutes combined should be equal to turned on lights
+                if(Integer.bitCount(hours)+Integer.bitCount(minutes)==turnedOn){
+                    String hh=String.valueOf(hours);
+                    String mm=(minutes<10?"0":"")+minutes;
+
+                    time.add(hh+":"+mm);
+
+
+
+                }
+
+            }
+
+        }
+     for(String s:time){
+         System.out.print(s+" ");
+     }
     }
 }
