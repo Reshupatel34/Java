1ea7cc3776019320e04c0dab03549f72da35d8ac
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-06-23 23:05:09 +0530

    "Animals Using OOPs inheritance overriding , extends keyword"

---
+++ src/com/engineering/oopsjava/animals/Dog.java
@@ -0,0 +1,7 @@
+package com.engineering.oopsjava.animals;
+
+public class Dog extends Animals {
+      public void sayHello(){
+          System.out.println("Woof Woof");
+      }
+}
---
+++ src/com/engineering/oopsjava/humans/Child.java
@@ -0,0 +1,8 @@
+package com.engineering.oopsjava.humans;
+
+public class Child extends Parent{
+    public Child(){
+//        hasSuperPowers=true;
+        System.out.println(" Child contructor called");
+    }
+}
