d86eada6ebf253e6c2650777ef775da7eb70c0ac
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-07-02 22:00:24 +0530

    "calculator"

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int m=sc.nextInt();
+//        System.out.println("Enter a sign : ");
+//        String sign=sc.next();
+//        if(sign.equals("*")){
+//            int mult=n*m;
+//            System.out.println("Mult : "+mult);
+//        }
+//        else if(sign.equals("/")){
+//            int mult=n/m;
+//            System.out.println("Div : "+mult);
+//        }
+//        else if(sign.equals("-")){
+//            int mult=n-m;
+//            System.out.println("Sub : "+mult);
+//        }
+//        else if(sign.equals("+")){
+//            int mult=n+m;
+//            System.out.println("Add : "+mult);
+//        }
+        int[] arr={1,2,3,4};
+
+    }
+}
