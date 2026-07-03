466d3457c947dfeb751be2bbdc0b540fa3cc4cb4
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-07-03 22:54:53 +0530

    "calculator"

---
+++ src/com/college/javainterface/ExtendingInterfaces.java
@@ -0,0 +1,12 @@
+
+package com.college.javainterface;
+import java.util.*;
+
+
+class ExtendingInterfaces{
+    public static void main(String[] args) {
+      ArrayList list=new ArrayList<>();
+      List list1=new ArrayList<>();
+
+    }
+}
